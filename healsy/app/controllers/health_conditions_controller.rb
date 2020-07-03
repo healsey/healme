021efb143641f4a9b26ecaaf234ccda48382bbd5
bashir2017 <bashiralhanshali@yahoo.com>
2020-07-02 23:20:21 -0400

    create welcome page and draft for recipes display

--- healsy/app/controllers/health_conditions_controller.rb
+++ healsy/app/controllers/health_conditions_controller.rb
@@ -4,7 +4,7 @@ class HealthConditionsController < ApplicationController
         # conditions = HealthCondition.all
         # render json: conditions
 
-        url = "https://api.edamam.com/search?q=''&app_id=63149c77&app_key=a850c45e6565a7cd23cf7065a04a774c&health=sugar-conscious"
+        url = "https://api.edamam.com/search?q=&app_id=63149c77&app_key=a850c45e6565a7cd23cf7065a04a774c&health=sugar-conscious"
         data = RestClient.get(url)
 
         # #url = 'https://api.nal.usda.gov/fdc/v1/foods/list?api_key=Dnp8tZTZkqEP5ZfoYlDFPa0CUZ3MRhF7PstcjpWu'
